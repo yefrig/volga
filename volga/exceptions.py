6d9708418f2eb48d475a8a0f04bae79fd894790f
Ecenaz Ozmen <eo2419@columbia.edu>
2020-12-10 13:22:59 -0500

    reformatting for black

--- volga/exceptions.py
+++ volga/exceptions.py
@@ -1,6 +1,10 @@
 class VolgaError(Exception):
     """Base class for all volga-related errors."""
 
+    pass
+
 
 class SerializationError(VolgaError):
     """Raised during serialization errors when parsing or building object instances."""
+
+    pass
